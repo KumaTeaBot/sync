21271ac22aa86e6eb45f39cc71234f4428b84639
KumaTea <KumaTea@outlook.com>
2023-12-26 22:05:23 +0800

    add workflow runs clearer

---
+++ clear.py
@@ -0,0 +1,30 @@
+import json
+import subprocess
+
+
+PROTECTED_RUNS = [
+    7051989759,
+]
+
+
+def get_workflow_runs():
+    command = 'gh run list --json displayTitle,databaseId'
+    result = subprocess.run(command, shell=True, capture_output=True)
+    result = result.stdout.decode('utf-8')
+    result = json.loads(result)
+    # skip the first one
+    result = result[1:]
+    return result
+
+
+def delete_workflow_runs():
+    runs = get_workflow_runs()
+    for run in runs:
+        if run['databaseId'] not in PROTECTED_RUNS:
+            command = f'gh run delete {run["databaseId"]}'
+            subprocess.run(command, shell=True)
+            print(f'Deleted run {run["databaseId"]}')
+
+
+if __name__ == '__main__':
+    delete_workflow_runs()
